c661bbfa4096f3d4f0f700899a00392cde4e5883
Misfit99 <amansabale007@gmail.com>
2022-05-09 14:47:31 +0530

    Products Resolver

---
+++ products/products.resolvers.js
@@ -0,0 +1,26 @@
+const productsModel = require('./products.model');
+
+module.exports = {
+    Query: {
+        products: () => {
+            return productsModel.getAllProducts();
+        },
+
+        productsByPrice: (_, args) => {
+            return productsModel.getAllProductsByPrice(args.min, args.max);
+        },
+
+        productsByID: (_, args) => {
+            return productsModel.getProductsByID(args.id);
+        }
+    },
+
+    Mutation: {
+        addNewProduct: (_, args) => {
+            return productsModel.addNewProduct(args.id, args.description, args.price);
+        },
+        addNewProductReview : (_, args) => {
+            return productsModel.addNewProductReview(args.id, args.rating, args.comment);
+        }
+    }
+};
